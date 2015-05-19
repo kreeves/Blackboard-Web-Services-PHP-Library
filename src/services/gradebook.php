3f33e19f7b321b1c303328f270800b0303d06ebf
Kyle Reeves <kreeves@gmail.com>
2015-05-19 12:50:50 -0500

    Add getGrades method to Gradebook service.

--- src/services/gradebook.php
+++ src/services/gradebook.php
@@ -16,6 +16,31 @@ class Gradebook extends Service {
 	public function __call($method, $args = null) {
 		return parent::buildBody($method, 'Gradebook', $args[0]);
 	}
+
+    public function getGrades($args)
+     {
+         $body = '<ns1:courseId>' . $args['courseId'] . '</ns1:courseId>';
+         $body .= '<ns1:filter>';
+
+
+         foreach ($args['filter'] as $key => $arg)
+         {
+             if( !is_array($arg) )
+             {
+                 $body .= '<ns1:' . $key . '>' . $arg . '</ns1:' . $key . '>';
+             } else {
+                     foreach( $arg as $sub_arg )
+                 {
+                             $body .= '<ns1:' . $key . '>' . $sub_arg . '</ns1:' . $key . '>';
+                         }
+             }
+
+         }
+
+         $body .= '</ns1:filter>';
+
+         return parent::buildBody("getGrades", "Gradebook", $body);
+     }
 }
 
 ?>
